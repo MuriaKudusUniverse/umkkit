7af44deac920349c17631f21961044553cdee1a7
Ardi Irfanto <ardi.irfanto@umk.ac.id>
2024-02-29 13:42:57 +0700

    UPDATE : Version n Fix Composer.JSON

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '0.0.1',
+        'version' => '0.0.1.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'umkdev/umkkit',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'umkdev/umkkit' => array(
+            'pretty_version' => '0.0.1',
+            'version' => '0.0.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
