8eca9e4c3903d34cb5cb0cd314ee781dc3d86e07
Ardi Irfanto <ardi.irfanto@umk.ac.id>
2024-02-29 13:12:08 +0700

    UPDATE : Add Function and Commands

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -1,16 +1,10 @@
 <?php
 
-namespace App\Console\Commands;
+namespace Umkdev\Umkkit\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Illuminate\Filesystem\Filesystem;
-use Illuminate\Support\Str;
-use RuntimeException;
-
-use function Laravel\Prompts\confirm;
-use function Laravel\Prompts\multiselect;
-use function Laravel\Prompts\select;
 
 class InstallCommand extends Command implements PromptsForMissingInput
 {
@@ -33,16 +27,59 @@ class InstallCommand extends Command implements PromptsForMissingInput
      *
      * @return int
      */
+
+    var $package_path = __DIR__ . '/../';
+    // var $package_path = __DIR__.'/../../../packages/Umkdev/umkkit/src/';
+
     public function handle()
     {
 
-        $choice = $this->choice('Pilih template yang anda gunakan:',['qompactui','sneat']);
+        // $this->info($this->package_path);
 
-        if($choice === 'qompactui'){
-            $this->info("QOMPACT UI");
-        } else if($choice === 'sneat'){
-            $this->info("SNEAT");
+
+        $choice = $this->choice('Pilih template yang anda gunakan:', ['qompactui', 'sneat']);
+
+        if ($choice === 'qompactui') {
+            $this->warn("Anda Memilih Template : Qompact UI");
+        } else if ($choice === 'sneat') {
+            $this->warn("Anda Memilih Template : Sneat");
         }
+        $this->warn("Proses Instalasi Template....");
+        $this->installTemplate($choice);
+        $this->installApp();
+        $this->installDatabase();
+        $this->installRoute();
 
+        $this->line('----------- Made By LSI -----------');
+        $this->info('Template berhasil di install');
+    }
+
+    private function installTemplate($template = 'qompactui')
+    {
+        (new Filesystem)->ensureDirectoryExists(resource_path('views'));
+        (new Filesystem)->ensureDirectoryExists(public_path(''));
+
+        (new Filesystem)->copyDirectory($this->package_path . 'Templates/' . $template . '/assets/', public_path('assets'));
+        (new Filesystem)->copyDirectory($this->package_path . 'Templates/' . $template . '/views/', resource_path('views'));
+    }
+    private function installApp()
+    {
+        $source_path = $this->package_path . 'App/';
+        (new Filesystem)->ensureDirectoryExists(app_path('Http'));
+        (new Filesystem)->ensureDirectoryExists(app_path('Models'));
+
+        (new Filesystem)->copyDirectory($source_path . 'Http/', app_path('Http'));
+        (new Filesystem)->copyDirectory($source_path . 'Models/', app_path('Models'));
+        (new Filesystem)->copyDirectory($source_path . 'Services/', app_path('Services'));
+    }
+    private function installRoute()
+    {
+        (new Filesystem)->ensureDirectoryExists(base_path('routes'));
+        (new Filesystem)->copy($this->package_path . 'Routes/web.php', base_path('routes/web.php'));
+    }
+    private function installDatabase()
+    {
+        (new Filesystem)->ensureDirectoryExists(base_path('database'));
+        (new Filesystem)->copyDirectory($this->package_path . 'Database/', base_path('database'));
     }
 }
--- src/UmkServiceProvider.php
+++ src/UmkServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace Umkdev\Umkkit;
 
 use Illuminate\Contracts\Support\DeferrableProvider;
 use Illuminate\Support\ServiceProvider;
